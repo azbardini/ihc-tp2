63e68ce856d5e55fb5fd4c921670ae9428f02712
Augusto Zanella Bardini <azbardini@gmail.com>
2021-03-05 16:08:24 -0300

    Add ex11

---
+++ ihc-1-1/app/src/main/java/com/bardni/ihc1/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.bardni.ihc1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<View>(R.id.inputButton)
+
+        button1.setOnClickListener {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+
+        val inputText1 = findViewById<EditText>(R.id.inputText1)
+        val inputText2 = findViewById<EditText>(R.id.inputText2)
+
+        val number1 = inputText1.text.toString().toIntOrNull()
+        val number2 = inputText2.text.toString().toIntOrNull()
+
+        val outputText = findViewById<TextView>(R.id.outputText)
+
+        if (number1 != null && number2!= null ){
+            outputText.text = (number1+number2).toString()
+        }
+
+    }
+
+
+
+}
